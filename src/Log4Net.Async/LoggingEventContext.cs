e7e2c93ebe4db91afac95c2dd898ab9f5d95f9be
Richard Collette <rcollette@yahoo.com>
2014-10-26 20:08:35 -0400

    Capture HttpContext in AsyncForwarder

---
+++ src/Log4Net.Async/LoggingEventContext.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Log4Net.Async
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Log4Net;
+    using log4net.Core;
+
+
+    internal class LoggingEventContext
+    {
+        public LoggingEventContext(LoggingEvent loggingEvent, object httpContext)
+        {
+            LoggingEvent = loggingEvent;
+            HttpContext = httpContext;
+        }
+
+        public LoggingEvent LoggingEvent { get; set; }
+        public object HttpContext { get; set; }
+    }
+}
