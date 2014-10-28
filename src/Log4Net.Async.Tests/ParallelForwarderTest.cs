baaac06a75e3ef46e472471d20800a3e5b360c24
rcollette@yahoo.com <rcollette@yahoo.com>
2014-10-28 13:23:03 -0400

    Created ParallelForwardingAppender, AsyncForwardingAppenderBase and associated unit tests for ParallelForwardingAppender.

---
+++ src/Log4Net.Async.Tests/ParallelForwarderTest.cs
@@ -0,0 +1,315 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Filter;
+using log4net.Repository;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading;
+
+namespace Log4Net.Async.Tests
+{
+    [TestFixture]
+    public class ParallelForwarderTest : IDisposable
+    {
+        private ParallelForwardingAppender asyncForwardingAppender;
+        private DebugAppender debugAppender;
+        private ILoggerRepository repository;
+        private ILog log;
+
+        [SetUp]
+        public void TestFixtureSetUp()
+        {
+            debugAppender = new DebugAppender();
+            debugAppender.ActivateOptions();
+
+            asyncForwardingAppender = new ParallelForwardingAppender();
+            asyncForwardingAppender.AddAppender(debugAppender);
+            asyncForwardingAppender.ActivateOptions();
+
+            repository = LogManager.CreateRepository(Guid.NewGuid().ToString());
+            BasicConfigurator.Configure(repository, asyncForwardingAppender);
+
+            log = LogManager.GetLogger(repository.Name, "TestLogger");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            LogManager.Shutdown();
+        }
+
+        [Test]
+        public void CanHandleNullLoggingEvent()
+        {
+            // Arrange
+
+            // Act
+            asyncForwardingAppender.DoAppend((LoggingEvent)null);
+            log.Info("SusequentMessage");
+            asyncForwardingAppender.Close();
+
+            // Assert - should not have had an exception from previous call
+            Assert.That(debugAppender.LoggedEventCount, Is.EqualTo(1), "Expected subsequent message only");
+            Assert.That(debugAppender.GetEvents()[0].MessageObject, Is.EqualTo("SusequentMessage"));
+        }
+
+        [Test]
+        public void CanHandleNullLoggingEvents()
+        {
+            // Arrange
+
+            // Act
+            asyncForwardingAppender.DoAppend((LoggingEvent[])null);
+            log.Info("SusequentMessage");
+            asyncForwardingAppender.Close();
+
+            // Assert - should not have had an exception from previous call
+            Assert.That(debugAppender.LoggedEventCount, Is.EqualTo(1), "Expected subsequent message only");
+            Assert.That(debugAppender.GetEvents()[0].MessageObject, Is.EqualTo("SusequentMessage"));
+        }
+
+        [Test]
+        public void CanHandleAppenderThrowing()
+        {
+            // Arrange
+            var badAppender = MockRepository.GenerateMock<IAppender>();
+            asyncForwardingAppender.AddAppender(badAppender);
+
+            badAppender
+                .Expect(ba => ba.DoAppend(null))
+                .IgnoreArguments()
+                .Throw(new Exception("Bad Appender"))
+                .Repeat.Twice();
+
+            // Act
+            log.Info("InitialMessage");
+            log.Info("SusequentMessage");
+            asyncForwardingAppender.Close();
+
+            // Assert
+            Assert.That(debugAppender.LoggedEventCount, Is.EqualTo(2));
+            Assert.That(debugAppender.GetEvents()[1].MessageObject, Is.EqualTo("SusequentMessage"));
+            badAppender.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void WillLogFastWhenThereIsASlowAppender()
+        {
+            const int testSize = 1000;
+
+            // Arrange
+            debugAppender.AppendDelay = TimeSpan.FromSeconds(30);
+            var watch = new Stopwatch();
+
+            // Act
+            watch.Start();
+            for (int i = 0; i < testSize; i++)
+            {
+                log.Error("Exception");
+            }
+            watch.Stop();
+
+            // Assert
+            Assert.That(debugAppender.LoggedEventCount, Is.EqualTo(0));
+            Assert.That(watch.ElapsedMilliseconds, Is.LessThan(testSize));
+            Console.WriteLine("Logged {0} errors in {1}ms", testSize, watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void WillNotOverflow()
+        {
+            const int testSize = 1000;
+
+            // Arrange
+            debugAppender.AppendDelay = TimeSpan.FromMilliseconds(1);
+            asyncForwardingAppender.BufferSize = 100;
+
+            // Act
+            for (int i = 0; i < testSize; i++)
+            {
+                log.Error("Exception");
+            }
+
+            Thread.Sleep(100);
+            asyncForwardingAppender.Close();
+
+            // Assert
+            Assert.That(debugAppender.LoggedEventCount, Is.EqualTo(testSize));
+        }
+
+        [Test]
+        public void WillTryToFlushBufferOnShutdown()
+        {
+            const int testSize = 250;
+
+            // Arrange
+            debugAppender.AppendDelay = TimeSpan.FromMilliseconds(1);
+
+            // Act
+            for (int i = 0; i < testSize; i++)
+            {
+                log.Error("Exception");
+            }
+
+            Thread.Sleep(50);
+
+            var numberLoggedBeforeClose = debugAppender.LoggedEventCount;
+            asyncForwardingAppender.Close();
+            var numberLoggedAfterClose = debugAppender.LoggedEventCount;
+
+            // Assert
+            Assert.That(numberLoggedBeforeClose, Is.LessThan(100));
+            Assert.That(numberLoggedAfterClose, Is.EqualTo(testSize));
+            Console.WriteLine("Flushed {0} events during shutdown", numberLoggedAfterClose - numberLoggedBeforeClose);
+        }
+
+        [Test, Explicit("Long-running")]
+        public void WillShutdownIfBufferCannotBeFlushedFastEnough()
+        {
+            const int testSize = 250;
+
+            // Arrange
+            var watch = new Stopwatch();
+            debugAppender.AppendDelay = TimeSpan.FromSeconds(1);
+
+            // Act
+            for (int i = 0; i < testSize; i++)
+            {
+                log.Error("Exception");
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            var numberLoggedBeforeClose = debugAppender.LoggedEventCount;
+
+            watch.Start();
+            asyncForwardingAppender.Close();
+            watch.Stop();
+
+            var numberLoggedAfterClose = debugAppender.LoggedEventCount;
+
+            // Assert
+            Assert.That(numberLoggedBeforeClose, Is.GreaterThan(0));
+            Assert.That(numberLoggedAfterClose, Is.GreaterThan(numberLoggedBeforeClose));
+            Assert.That(numberLoggedAfterClose, Is.LessThan(testSize));
+            Assert.That(watch.ElapsedMilliseconds, Is.LessThan(7000), "should be around 5s + the duration of the last append + disposal");
+            var events = debugAppender.GetEvents();
+            var evnt = events[events.Length - 1];
+            Assert.That(evnt.MessageObject, Is.EqualTo("The ParallelForwardingAppender buffer was not able to be flushed before timeout occurred."));
+            Console.WriteLine("Flushed {0} events during shutdown which lasted {1}ms", numberLoggedAfterClose - numberLoggedBeforeClose, watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void ThreadContextPropertiesArePreserved()
+        {
+            // Arrange
+            ThreadContext.Properties["TestProperty"] = "My Value";
+            Assert.That(asyncForwardingAppender.Fix & FixFlags.Properties, Is.EqualTo(FixFlags.Properties), "Properties must be fixed if they are to be preserved");
+
+            // Act
+            log.Info("Information");
+            asyncForwardingAppender.Close();
+
+            // Assert
+            var lastLoggedEvent = debugAppender.GetEvents()[0];
+            Assert.That(lastLoggedEvent.Properties["TestProperty"], Is.EqualTo("My Value"));
+        }
+
+        [Test]
+        public void MessagesExcludedByFilterShouldNotBeAppended()
+        {
+            // Arrange
+            var levelFilter =
+                new LevelRangeFilter
+                {
+                    LevelMin = Level.Warn,
+                    LevelMax = Level.Error,
+                };
+
+            asyncForwardingAppender.AddFilter(levelFilter);
+
+            // Act
+            log.Info("Info");
+            log.Warn("Warn");
+            log.Error("Error");
+            log.Fatal("Fatal");
+
+            asyncForwardingAppender.Close();
+
+            //Assert
+            Assert.That(debugAppender.LoggedEventCount, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void HelperCanGenerateLoggingEventWithAllProperties()
+        {
+            // Arrange
+            var helper = new LoggingEventHelper("TestLoggerName", FixFlags.All);
+            ThreadContext.Properties["MyProperty"] = "MyValue";
+            var exception = new Exception("SomeError");
+
+            var stackFrame = new StackFrame(0);
+            var currentUser = WindowsIdentity.GetCurrent();
+            var loggingTime = DateTime.Now; // Log4Net does not seem to be using UtcNow
+
+            // Act
+            var loggingEvent = helper.CreateLoggingEvent(Level.Emergency, "Who's on live support?", exception);
+            Thread.Sleep(50); // to make sure the time stamp is actually captured
+
+            // Assert
+            Assert.That(loggingEvent.Domain, Is.EqualTo(AppDomain.CurrentDomain.FriendlyName), "Domain");
+            Assert.That(loggingEvent.Identity, Is.Empty, "Identity: always empty for some reason");
+            Assert.That(loggingEvent.UserName, Is.EqualTo(currentUser == null ? String.Empty : currentUser.Name), "UserName");
+            Assert.That(loggingEvent.ThreadName, Is.EqualTo(Thread.CurrentThread.Name), "ThreadName");
+
+            Assert.That(loggingEvent.Repository, Is.Null, "Repository: Helper does not have access to this");
+            Assert.That(loggingEvent.LoggerName, Is.EqualTo("TestLoggerName"), "LoggerName");
+
+            Assert.That(loggingEvent.Level, Is.EqualTo(Level.Emergency), "Level");
+            Assert.That(loggingEvent.TimeStamp, Is.EqualTo(loggingTime).Within(TimeSpan.FromMilliseconds(5)), "TimeStamp");
+            Assert.That(loggingEvent.ExceptionObject, Is.EqualTo(exception), "ExceptionObject");
+            Assert.That(loggingEvent.MessageObject, Is.EqualTo("Who's on live support?"), "MessageObject");
+
+            Assert.That(loggingEvent.LocationInformation.MethodName, Is.EqualTo(stackFrame.GetMethod().Name), "LocationInformation");
+            Assert.That(loggingEvent.Properties["MyProperty"], Is.EqualTo("MyValue"), "Properties");
+        }
+
+        private bool _disposed = false;
+
+        //Implement IDisposable.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (asyncForwardingAppender != null)
+                    {
+                        asyncForwardingAppender.Dispose();
+                        asyncForwardingAppender = null;
+                    }
+                }
+                // Free your own state (unmanaged objects).
+                // Set large fields to null.
+                _disposed = true;
+            }
+        }
+
+        // Use C# destructor syntax for finalization code.
+        ~ParallelForwarderTest()
+        {
+            // Simply call Dispose(false).
+            Dispose(false);
+        }
+    }
+}
---
+++ src/Log4Net.Async/ParallelForwardingAppender.cs
@@ -0,0 +1,255 @@
+﻿namespace Log4Net.Async
+{
+    using log4net.Core;
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An asynchronous appender based on <see cref="BlockingCollection'T"/>
+    /// </summary>
+    public class ParallelForwardingAppender : AsyncForwardingAppenderBase, IDisposable
+    {
+        #region Private Members
+
+        private const int DefaultBufferSize = 1000;
+        private BlockingCollection<LoggingEventContext> _loggingEvents;
+        private CancellationTokenSource _loggingCancelationTokenSource;
+        private CancellationToken _loggingCancelationToken;
+        private Task _loggingTask;
+        private Double _shutdownFlushTimeout = 5;
+        private TimeSpan _shutdownFlushTimespan = TimeSpan.FromSeconds(5);
+        private static readonly Type ThisType = typeof(ParallelForwardingAppender);
+        private volatile bool shutDownRequested;
+        private int? bufferSize = DefaultBufferSize;
+
+        #endregion Private Members
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the number of LoggingEvents that will be buffered.  Set to null for unlimited.
+        /// </summary>
+        public override int? BufferSize
+        {
+            get { return bufferSize; }
+            set { bufferSize = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the time period in which the system will wait for appenders to flush before canceling the background task.
+        /// </summary>
+        public Double ShutdownFlushTimeout
+        {
+            get
+            {
+                return _shutdownFlushTimeout;
+            }
+            set
+            {
+                _shutdownFlushTimeout = value;
+            }
+        }
+
+        protected override string InternalLoggerName
+        {
+            get
+            {
+                {
+                    return "ParallelForwardingAppender";
+                }
+            }
+        }
+
+        #endregion Properties
+
+        #region Startup
+
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+            _shutdownFlushTimespan = TimeSpan.FromSeconds(_shutdownFlushTimeout);
+            StartForwarding();
+        }
+
+        private void StartForwarding()
+        {
+            if (shutDownRequested)
+            {
+                return;
+            }
+            //Create a collection which will block the thread and wait for new entries
+            //if the collection is empty
+            if (BufferSize.HasValue && BufferSize > 0)
+            {
+                _loggingEvents = new BlockingCollection<LoggingEventContext>(BufferSize.Value);
+            }
+            else
+            {
+                //No limit on the number of events.
+                _loggingEvents = new BlockingCollection<LoggingEventContext>();
+            }
+            //The cancellation token is used to cancel a running task gracefully.
+            _loggingCancelationTokenSource = new CancellationTokenSource();
+            _loggingCancelationToken = _loggingCancelationTokenSource.Token;
+            _loggingTask = new Task(SubscriberLoop, _loggingCancelationToken);
+            _loggingTask.Start();
+        }
+
+        #endregion Startup
+
+        #region Shutdown
+
+        private void CompleteSubscriberTask()
+        {
+            shutDownRequested = true;
+            if (_loggingEvents == null || _loggingEvents.IsAddingCompleted)
+            {
+                return;
+            }
+            //Don't allow more entries to be added.
+            _loggingEvents.CompleteAdding();
+            //Allow some time to flush
+            Thread.Sleep(_shutdownFlushTimespan);
+            //Cancel the task
+            if (!_loggingCancelationToken.IsCancellationRequested)
+            {
+                _loggingCancelationTokenSource.Cancel();
+            }
+            if (!_loggingEvents.IsCompleted)
+            {
+                ForwardInternalError("The ParallelForwardingAppender buffer was not able to be flushed before timeout occurred.", null, ThisType);
+            }
+        }
+
+        protected override void OnClose()
+        {
+            CompleteSubscriberTask();
+            Dispose();
+            base.OnClose();
+        }
+
+        #endregion Shutdown
+
+        #region Appending
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (_loggingEvents == null || _loggingEvents.IsAddingCompleted || loggingEvent == null)
+            {
+                return;
+            }
+
+            loggingEvent.Fix = Fix;
+            _loggingEvents.Add(new LoggingEventContext(loggingEvent, HttpContext));
+        }
+
+        protected override void Append(LoggingEvent[] loggingEvents)
+        {
+            if (_loggingEvents == null || _loggingEvents.IsAddingCompleted || loggingEvents == null)
+            {
+                return;
+            }
+
+            foreach (var loggingEvent in loggingEvents)
+            {
+                Append(loggingEvent);
+            }
+        }
+
+        #endregion Appending
+
+        #region Forwarding
+
+        /// <summary>
+        /// Iterates over a BlockingCollection containing LoggingEvents.
+        /// </summary>
+        private void SubscriberLoop()
+        {
+            Thread.CurrentThread.Name = String.Format("{0} ParallelForwardingAppender Subscriber Task", Name);
+            //The task will continue in a blocking loop until
+            //the queue is marked as adding completed, or the task is canceled.
+            while (!(_loggingCancelationToken.IsCancellationRequested || _loggingEvents.IsCompleted))
+            {
+                try
+                {
+                    //This call blocks until an item is available.
+                    var entry = _loggingEvents.Take(_loggingCancelationToken);
+                    //Check if there is an entry since the take may have been canceled.
+                    if (entry != null)
+                    {
+                        HttpContext = entry.HttpContext;
+                        ForwardLoggingEvent(entry.LoggingEvent, ThisType);
+                    }
+                }
+                catch (ThreadAbortException ex)
+                {
+                    //Thread abort may occur on domain unload.
+                    ForwardInternalError("Subscriber task was aborted.", ex, ThisType);
+                    CompleteSubscriberTask();
+                    //The exception is swallowed because we don't want the client application
+                    //to halt due to a logging issue.
+                }
+                catch (Exception ex)
+                {
+                    //On exception, try to log the exception
+                    ForwardInternalError("Subscriber task error in forwarding loop.", ex, ThisType);
+                    CompleteSubscriberTask();
+                    //The exception is swallowed because we don't want the client application
+                    //to halt due to a logging issue.
+                }
+            }
+            //The following is necessary to move the task into the canceled state,
+            //otherwise it would be left in a faulted state.   A fault state
+            //would indicate that the logging thread did not complete gracefully.
+            if ((_loggingCancelationToken.IsCancellationRequested))
+            {
+                _loggingCancelationToken.ThrowIfCancellationRequested();
+            }
+        }
+
+        #endregion Forwarding
+
+        #region IDisposable Implementation
+
+        private bool _disposed = false;
+
+        //Implement IDisposable.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_loggingEvents != null)
+                    {
+                        _loggingEvents.Dispose();
+                        _loggingEvents = null;
+                    }
+                    if (_loggingCancelationTokenSource != null)
+                    {
+                        _loggingCancelationTokenSource.Dispose();
+                        _loggingCancelationTokenSource = null;
+                    }
+                }
+                _disposed = true;
+            }
+        }
+
+        // Use C# destructor syntax for finalization code.
+        ~ParallelForwardingAppender()
+        {
+            // Simply call Dispose(false).
+            Dispose(false);
+        }
+
+        #endregion IDisposable Implementation
+    }
+}
