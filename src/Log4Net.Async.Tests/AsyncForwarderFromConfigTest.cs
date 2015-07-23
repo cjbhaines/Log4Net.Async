3864bfe5bc31823959d2c998d1bcf244e15b6e56
Chris Haines <hainesc@live.co.uk>
2015-07-23 13:08:35 +0100

    Add additional tests for #13. Make sure we use XmlConfigurator to parse config file and override bufferSize

---
+++ src/Log4Net.Async.Tests/AsyncForwarderFromConfigTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using log4net;
+using log4net.Config;
+using NUnit.Framework;
+
+namespace Log4Net.Async.Tests
+{
+    [TestFixture]
+    public class AsyncForwarderFromConfigTest
+    {
+        [Test]
+        public void BufferSizeIsCorrectlyApplied()
+        {
+            // Arrange
+            XmlConfigurator.Configure();
+            var log = LogManager.GetLogger("TestLogger");
+            
+            // Act
+            var appenders = log.Logger.Repository.GetAppenders();
+
+            // Assert
+            var asyncForwarder = appenders.FirstOrDefault(x => x is AsyncForwardingAppender) as AsyncForwardingAppender;
+            Assert.That(asyncForwarder, Is.Not.Null);
+            const int bufferSizeInConfigFile = 2000;
+            Assert.That(asyncForwarder.BufferSize, Is.EqualTo(bufferSizeInConfigFile));
+
+            LogManager.Shutdown();
+        }
+    }
+}
---
+++ src/Log4Net.Async.Tests/ParallelForwarderFromConfigTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using log4net;
+using log4net.Config;
+using NUnit.Framework;
+
+namespace Log4Net.Async.Tests
+{
+    [TestFixture]
+    public class ParallelForwarderFromConfigTest
+    {
+        [Test]
+        public void BufferSizeIsCorrectlyApplied()
+        {
+            // Arrange
+            XmlConfigurator.Configure();
+            var log = LogManager.GetLogger("TestLogger");
+            
+            // Act
+            var appenders = log.Logger.Repository.GetAppenders();
+
+            // Assert
+            var parallelForwardingAppender = appenders.FirstOrDefault(x => x is ParallelForwardingAppender) as ParallelForwardingAppender;
+            Assert.That(parallelForwardingAppender, Is.Not.Null);
+            const int bufferSizeInConfigFile = 2000;
+            Assert.That(parallelForwardingAppender.BufferSize, Is.EqualTo(bufferSizeInConfigFile));
+
+            LogManager.Shutdown();
+        }
+    }
+}
